b0059447c7681af7912f3a338e3a9266f2128fa3
unknown <samirphanuel@gmail.com>
2024-12-20 20:55:48 +0100

    ok

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -15,20 +15,28 @@ export const Skills = () => {
         },
         {
             logo: facebook,
+            text: 'Python'
+        },
+        {
+            logo: facebook,
             text: 'React Js'
         },
         {
             logo: facebook,
             text: 'Next Js'
         },
+        {
+            logo: facebook,
+            text: 'Tailwind Css'
+        },
     ]
     return (
-        <div className='md:px-[80px] md:py-[60px] px-[16px] py-[40px]'>
-            <div className='md:px-[32px] md:space-y-[20px]'>
+        <div className='py-10 px-4'>
+            <div className='space-y-5'>
                 <h1 className='display_text text-center'>Mes <span className='font-extrabold'>Compétences</span></h1>
-                <div className='py-[20px] md:flex md:justify-center md:gap-8 gap-[20px] grid grid-cols-2'>
-                    {skills.map(skill => (
-                        <SkillCard logo={skill.logo} text={skill.text} key={skill.text}/>
+                <div className='grid grid-cols-2 gap-5 sm:grid-cols-3 sm:w-[524.5px] sm:mx-auto'>
+                    {skills.map((skill, index) => (
+                        <SkillCard logo={skill.logo} text={skill.text} key={index}/>
                     ))}
                 </div>
             </div>
@@ -42,7 +50,7 @@ type SkillCardProps = {
 }
 function SkillCard({logo, text}: SkillCardProps) {
     return (
-        <div className='md:w-[186px] md:h-[186px] p-[24px] rounded-[4px] border border-primary flex flex-col md:gap-[32px] items-center'>
+        <div className='w-[161.5px] h-[160px] p-6 flex flex-col gap-8 rounded-[4px] border border-black'>
             <Image src={logo} alt='logo' className='w-[56px] h-[56px]'/>
             <span className='h5 font-semibold'> {text} </span>
         </div>
