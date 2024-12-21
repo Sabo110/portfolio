9444ccac54fa0a6178a93cf3fc9660c1647ff829
unknown <samirphanuel@gmail.com>
2024-12-21 17:25:29 +0100

    ok projects

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import { Sora } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 const sora = Sora({
   variable: "--font-sora",
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${sora.variable} antialiased`}
+        className={`${sora.variable} antialiased`}
       >
         {children}
       </body>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import React from 'react'
 import img1 from "@/public/img1.png"
 import img2 from "@/public/img2.png"
+import img3 from "@/public/img3.svg"
+import img4 from "@/public/img4.svg"
+
 import { cn } from '@/lib/utils'
 import { ExternalLink } from 'lucide-react';
 
@@ -11,27 +14,27 @@ export const Projects = () => {
         {
             title: 'Site internet pour une épicerie',
             demo: 'https://www.epicerie-mama-vercel.app',
-            description: "Site quipresente les differents produits d'une épicerie",
-            image: img1,
+            description: "Site quipresente les differents produits d'une épicerie.",
+            image: img3,
             number: '01'
         },
         {
             title: 'blog sur le developpement personnel',
             demo: 'https://www.epicerie-mama-vercel.app',
-            description: "Blog qui présente quelques articles sur le developpment personne, la motivation et les relations homme-femme",
-            image: img2,
-            order: 'md:order-2',
+            description: "Blog qui présente quelques articles sur le developpment personne, la motivation et les relations homme-femme.",
+            image: img4,
+            order: 'sm:order-last',
             number: '02'
         },
 
     ]
     return (
-        <div className='md:px-[80px] md:py-[60px] px-[16px] py-[40px] bg-primary text-white' >
-            <div className='px-[32px] space-y-[20px]'>
+        <div className='md:px-[16px] md:py-[40px] px-[16px] py-[40px] bg-primary text-white' >
+            <div className='lg:px-[32px] space-y-[20px]'>
                 <h1 className='text-center display_text'>Mes <span className='font-extrabold'>Projets</span></h1>
-                {Projects.map((project, index) => (
-                    <ProjectCrad project={project} key={index} />
-                ))}
+                    {Projects.map((project, index) => (
+                        <ProjectCrad project={project} key={index} />
+                    ))}
             </div>
         </div>
     )
@@ -49,14 +52,14 @@ type ProjectCradProps = {
 }
 function ProjectCrad({ project }: ProjectCradProps) {
     return (
-        <div className='flex md:items-center md:gap-[40px] flex-col gap-[28px]'>
-            <div className={cn('md:w-[594px] md:h-[476px] h-[398px] w-[300px] md:ms-auto md:flex md:flex-col md:justify-center', project.order)}>
-                <Image src={project.image} alt='image de couverture du projet' className='object-fill h-full w-full' />
+        <div className='sm:py-5 sm:flex sm:items-center sm:gap-5  sm:min-w-[608px] sm:max-w-[700px] sm:mx-auto md:min-w-[716px] md:max-w-[800px] lg:min-w-[908px] xl:min-w-[1164px]'>
+            <div className={cn('sm:w-[300px] sm:h-[200px] md:w-[350px] md:h-[250px] xl:w-[450px] xl:h-[350px]', project.order)}>
+                <Image src={project.image} alt='image de couverture du projet' className='object-cover w-full h-full' />
             </div>
-            <div className='flex flex-col gap-[28px]'>
+            <div className='flex flex-col gap-7  sm:w-[108px] grow'>
                 <span className='display_text font-extrabold'> {project.number} </span>
-                <span className='h3 font-semibold'> {project.title} </span>
-                <p> {project.description} </p>
+                <span className='heading_h2 lg:font-bold font-extrabold'> {project.title} </span>
+                <p className=''> {project.description} </p>
                 <a href={project.demo}>
                     <ExternalLink size={30} />
                 </a>
