a7979b6818bbb3c297f091c647c4e3b6f943108f
unknown <samirphanuel@gmail.com>
2024-12-22 21:55:02 +0100

    ok

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,7 +3,7 @@ import { Formulaire } from './Form'
 
 export const Contact = () => {
   return (
-    <div className='px-4 py-10'>
+    <div className='px-4 py-10' id='contact_me'>
         <div className='py-5 space-y-5 w-[343px] sm:w-[380px] mx-auto'>
           <h1 className='text-center display_text'>Me <span className='font-extrabold'>Contacter</span></h1>
           <Formulaire />
