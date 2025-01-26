a11a97d6a1a28cadc8182bee0daae366612646e6
unknown <samirphanuel@gmail.com>
2025-01-26 17:09:02 +0100

    ok

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,9 +1,9 @@
 import { StaticImport } from 'next/dist/shared/lib/get-img-props'
 import Image from 'next/image'
 import React from 'react'
-import img3 from "@/public/img3.svg"
-import img4 from "@/public/img4.svg"
-import easyorder from "@/public/capture-easy-order.png"
+import easyorder from "@/public/easyorder.jpg"
+import epicerie from "@/public/epicerie.jpg"
+import blog from "@/public/blog.jpg"
 
 import { cn } from '@/lib/utils'
 import { ExternalLink } from 'lucide-react';
@@ -22,14 +22,14 @@ export const Projects = () => {
             title: 'Site internet pour une épicerie',
             demo: 'https://site-epicerie-pour-mama.vercel.app',
             description: "Site quipresente les differents produits d'une épicerie.",
-            image: img3,
+            image: epicerie,
             number: '02'
         },
         {
             title: 'blog sur le developpement personnel',
             demo: 'https://the-mind-set.netlify.app',
             description: "Blog qui présente quelques articles sur le developpment personne, la motivation et les relations homme-femme.",
-            image: img4,
+            image: blog,
             order: 'sm:order-last',
             number: '03'
         },
@@ -61,7 +61,7 @@ function ProjectCrad({ project }: ProjectCradProps) {
     return (
         <div className='sm:py-5 sm:flex sm:items-center sm:gap-5  sm:min-w-[608px] sm:max-w-[700px] sm:mx-auto md:min-w-[716px] md:max-w-[800px] lg:min-w-[908px] xl:min-w-[1164px]'>
             <div className={cn('h-[350px] sm:w-[300px] sm:h-[200px] md:w-[350px] md:h-[250px] xl:w-[450px] xl:h-[350px]', project.order)}>
-                <Image src={project.image} alt='image de couverture du projet' className='object-cover w-full h-full' />
+                <Image src={project.image} alt='image de couverture du projet' className='object-cover w-full h-full' placeholder='blur'/>
             </div>
             <div className='flex flex-col gap-7  sm:w-[108px] grow'>
                 <span className='display_text font-extrabold'> {project.number} </span>
